e549d049c3c77ab9eaa7746e3b1560ba098f70c3
Ian Robertson <Ianmr08@gmail.com>
2017-12-22 10:54:21 -0600

    commit initial setup mode

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 9999;
+
+app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(port, function () {
+    console.log('server is up on:', port);
+})
