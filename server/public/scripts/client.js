23787070928ff596503e371e34cdf2b1e293d190
Ian Robertson <Ianmr08@gmail.com>
2017-12-22 11:38:30 -0600

    create cancel button and guess log class

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,8 +1,23 @@
+class GuessLog {
+    constructor (guess1, guess2, guess3, guess4, max) {
+        this.guess1 = guess1;
+        this.guess2 = guess2;
+        this.guess3 = guess3;
+        this.guess4 = guess4;
+        this.max = max;
+    }
+    // highLow (num) {
+    //     if (this.guess1 > num)
+    // }
+}
+
 $(document).ready(start);
 
 // event listeners
 function start () {
     $('#startGame').on('click', clickStart);
+    $('#mainContainer').on('click', '#cancelGame', cancel);
+    $('#mainContainer').on('click', '#submitUserGuess', submitGuess);
 }
 
 function clickStart () {
@@ -10,15 +25,24 @@ function clickStart () {
 
 //    let $input = $('<input type="number>');
     let $container = $('#mainContainer');
-   $container.append('<div><label>Player 1</label><input type="number" id="playerOne"></div>')
-   $container.append('<div><label>Player 2</label><input type="number" id="playerTwo"></div>')
-   $container.append('<div><label>Player 3</label><input type="number" id="playerThree"></div>')
-   $container.append('<div><label>Player 4</label><input type="number" id="playerFour"></div>')
+   $container.append('<div id="one"><label>Player 1</label><input type="number" id="playerOne"></div>')
+   $container.append('<div id="two"><label>Player 2</label><input type="number" id="playerTwo"></div>')
+   $container.append('<div id="three"><label>Player 3</label><input type="number" id="playerThree"></div>')
+   $container.append('<div id="four"><label>Player 4</label><input type="number" id="playerFour"></div>')
    $container.append('<div><button id="submitUserGuess">Submit Guess</button><button id="cancelGame">Quit Loser</button></div>')
-   $(this).remove();
+   $(this).toggle();
    // get dropdown value
-}
+}// end clickStart
 
-function cancelSubmit () {
-    
+//cancel button functions 
+function cancel () {
+console.log('cancel button has been clicked');
+// $('#statContainer').append();
+$('#startGame').toggle();
+$('#mainContainer').empty();
+}// end cancel Submit
+
+// submit button functions 
+function submitGuess () {
+    console.log('submit button clicked');
 }
