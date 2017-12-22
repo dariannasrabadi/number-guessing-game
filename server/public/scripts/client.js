78c803f6e6b234ce04682255ea38c4a8e5c9258d
Ian Robertson <Ianmr08@gmail.com>
2017-12-22 11:55:47 -0600

    finished Class and logged all information on inputs to the console.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -10,7 +10,7 @@ class GuessLog {
     //     if (this.guess1 > num)
     // }
 }
-
+let maxNum; // used to store max number selected
 $(document).ready(start);
 
 // event listeners
@@ -30,7 +30,11 @@ function clickStart () {
    $container.append('<div id="three"><label>Player 3</label><input type="number" id="playerThree"></div>')
    $container.append('<div id="four"><label>Player 4</label><input type="number" id="playerFour"></div>')
    $container.append('<div><button id="submitUserGuess">Submit Guess</button><button id="cancelGame">Quit Loser</button></div>')
+   maxNum = $('#dropDown option:selected').text();
+   console.log('this is the max num upon click start:', maxNum);
+   
    $(this).toggle();
+   $('#dropDown').toggle();
    // get dropdown value
 }// end clickStart
 
@@ -39,10 +43,22 @@ function cancel () {
 console.log('cancel button has been clicked');
 // $('#statContainer').append();
 $('#startGame').toggle();
+$('#dropDown').toggle();
 $('#mainContainer').empty();
 }// end cancel Submit
 
 // submit button functions 
 function submitGuess () {
     console.log('submit button clicked');
+    let userInput = new GuessLog($('#playerOne').val(), $('#playerTwo').val(), $('#playerThree').val(), $('#playerFour').val(), maxNum);
+    console.log(userInput);
 }
+
+// To DO
+// round number = every submit click
+// total guess counter = total number of guesses overall
+// create max number indicator = indicates the max number on page so dummys don't forget.
+// high or low
+
+//after server. 
+// reset button to restart game upon winning.  
